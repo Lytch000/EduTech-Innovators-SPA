0981a4c28ad54061a8a9606ceeb814d38c7b8d30
Seba Olguin <juanolguin000@gmail.com>
2025-05-18 02:01:05 -0400

    actuaizacion de estructura

--- src/main/java/com/EduTech/model/Client.java
+++ src/main/java/com/EduTech/model/Client.java
@@ -1,19 +1,34 @@
-package com.EduTech.example.model;
+package com.EduTech.model;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import jakarta.persistence.*;
 
+@Entity
+@Table(name = "cliente")
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
 public class Client {
 
-    private int id;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(unique = true, length = 13, nullable = false)
     private String rut;
+
+    @Column(nullable = false)
     private String nombre;
+
+    @Column(nullable = false)
     private int edad;
+
+    @Column(nullable = false)
     private String email;
+
+    @Column(nullable = false)
     private int celular;
 
 }
